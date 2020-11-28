b3a8095ecaec3ce0a1bdc9aa32997b0a48932f93
MatiasGutierrez1 <52360472+MatiasGutierrez1@users.noreply.github.com>
2020-11-27 22:48:15 -0300

    Práctica de Vue CLI en tutorial (tabla de frutas)

---
+++ Tutorial Vue.js/Vue/proyecto-2/src/components/Cabecera.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center">
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <h1>Frutas</h1>
+  </div>
+</template>
+
+<script>
+
+  import {mapState} from 'vuex';
+
+  export default {
+    name: 'Cabecera',
+
+  }
+</script>
---
+++ Tutorial Vue.js/Vue/proyecto-2/src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    frutas: [
+      {nombre: 'Manzana', cantidad: 0},
+      {nombre: 'Pera', cantidad: 0},
+      {nombre: 'Naranja', cantidad: 0}
+    ]
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  modules: {
+    
+  }
+})
