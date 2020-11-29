22c346244df1009cb19fce29c596c5a027b0a421
MatiasGutierrez1 <52360472+MatiasGutierrez1@users.noreply.github.com>
2020-11-28 23:58:38 -0300

    Todas las pestañas de mati en borrador
    Ya están las 6 que me correspondían, están mega al lote aún pero se pueden asociar cosas a los botones y todo eso

--- Creandolo/frontmati2/src/views/PremiosTrabajador.vue
+++ Creandolo/frontmati2/src/views/PremiosTrabajador.vue
@@ -1,6 +1,63 @@
 <template>
-  <div>
-
+  <div class="py-3 bg-success text-center">
+    <b-row>
+      <b-col cols="12" md="7">
+        <p>Nombre</p>
+      </b-col>
+      <b-col cols="12" md="4">
+        <p>Puntaje requerido</p>
+      </b-col>
+    </b-row>
+    <ul class="list-group">
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <b-card 
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-top
+          >  
+          <p>Premio</p>
+          <b-button variant="info" href="#">Descripción</b-button>
+        </b-card>
+        <b-col cols="12" md="2">
+          <p>Premio 1</p>
+        </b-col>
+        <b-col cols="12" md="4">
+          <p>800</p>
+        </b-col>
+        <button>Canjear</button>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <b-card 
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-top
+          >  
+          <p>Premio</p>
+          <b-button variant="info" href="#">Descripción</b-button>
+        </b-card>
+        <b-col cols="12" md="2">
+          <p>Premio 2</p>
+        </b-col>
+        <b-col cols="12" md="4">
+          <p>450</p>
+        </b-col>
+        <button>Canjear</button>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <b-card 
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-top
+          >  
+          <p>Premio</p>
+          <b-button variant="info" href="#">Descripción</b-button>
+        </b-card>
+        <b-col cols="12" md="2">
+          <p>Premio 3</p>
+        </b-col>
+        <b-col cols="12" md="4">
+          <p>300</p>
+        </b-col>
+        <button>Canjear</button>
+      </li>
+    </ul>
   </div>
 </template>
 
--- Creandolo/frontmati2/src/views/UsuariosPlanificador.vue
+++ Creandolo/frontmati2/src/views/UsuariosPlanificador.vue
@@ -1,6 +1,58 @@
 <template>
-  <div>
-
+  <div class="py-3 bg-success text-center">
+    <b-container>
+      <input type="text" class="form-control"></input>
+    </b-container>
+    <b-row>
+      <b-col cols="12" md="7">
+        <p>Usuarios</p>
+      </b-col>
+      <b-col cols="12" md="4">
+        <p>Puntajes</p>
+      </b-col>
+    </b-row>
+    <ul class="list-group">
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <b-col cols="12" md="2">
+          <p>Trabajador 1</p>
+        </b-col>
+        <b-col cols="12" md="4">
+          <p>5000</p>
+        </b-col>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <b-col cols="12" md="2">
+          <p>Trabajador 2</p>
+        </b-col>
+        <b-col cols="12" md="4">
+          <p>4630</p>
+        </b-col>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <b-col cols="12" md="2">
+          <p>Trabajador 3</p>
+        </b-col>
+        <b-col cols="12" md="4">
+          <p>4450</p>
+        </b-col>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <b-col cols="12" md="2">
+          <p>Trabajador 4</p>
+        </b-col>
+        <b-col cols="12" md="4">
+          <p>4110</p>
+        </b-col>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <b-col cols="12" md="2">
+          <p>Trabajador 5</p>
+        </b-col>
+        <b-col cols="12" md="4">
+          <p>3800</p>
+        </b-col>
+      </li>
+    </ul>
   </div>
 </template>
 
