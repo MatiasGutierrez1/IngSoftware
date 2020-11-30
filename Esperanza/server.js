07e70352cb0150aa08923b4eb04f95f4d55c2bb0
DavidSalas97 <55164712+DavidSalas97@users.noreply.github.com>
2020-11-30 02:47:21 -0400

    avance2

--- Esperanza/server.js
+++ Esperanza/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+const db = require('./models');
 
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -15,14 +16,17 @@ app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  });
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
+require("./routes/tutorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
